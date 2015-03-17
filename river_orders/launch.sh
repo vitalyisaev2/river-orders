fd0a97d4d5d35738730e47226f6e9ee143d8c62e
Vitaly Isaev <vitalyisaev2@gmail.com>
2015-03-18 02:40:06 +0300

    Improved the stacking algorithm

--- river_orders/launch.sh
+++ river_orders/launch.sh
@@ -1,1 +1,1 @@
-docker run --privileged -it --rm -v /home/vitaly/progs/own/river_orders:/home/vitaly/project:ro river_orders bash
+docker run --privileged -it --rm -v `pwd`:/home/vitaly/project:ro river_orders bash
--- scripts/build_river_network.py
+++ scripts/build_river_network.py
@@ -2,6 +2,7 @@
 # -*- coding: utf8 -*-
 
 import sys
+import re
 
 import pandas as pd
 import numpy as np
@@ -10,6 +11,45 @@ pd.set_option('display.width', 160)
 
 if __debug__:
     _df = None
+
+class RiverStack(list):
+    patterns = [
+        ur'^протока р. ([а-яА-Я-]+)$',
+    ]
+    def __init__(self):
+        self.rivers = []
+
+    def __str__(self):
+        return "<-".join(self.rivers)
+
+    def __getitem__(self, index):
+        return self.rivers[index]
+
+    def __len__(self):
+        return len(self.rivers)
+
+    def push(self, item):
+        return self.rivers.append(item)
+
+    def pop(self):
+        return self.rivers.pop()
+
+    def __contains__(self, dest):
+        for river in self.rivers:
+            if dest == river:
+                return True
+
+        unicode_rivers = [r.decode('utf8') for r in self.rivers]
+
+        for pattern in self.patterns:
+            m = re.search(pattern, dest.decode('utf8'))
+            if m:
+                name = m.groups()[0]
+                #exists = name in self.rivers
+                exists = name in unicode_rivers
+                print(u"\t'{}' -> '{}'; exists: {}".format(pattern, name, exists))
+                if exists:
+                    return True
 
 
 def prepare(df):
@@ -45,30 +85,36 @@ def prepare(df):
 
 
 def construct(df):
-    stack = []
-    #for index, row in df.iterrows():
-    for index, row in list(df.iterrows())[:20]:
+    stack = RiverStack()
+    for index, row in df.iterrows():
+    #for index, row in list(df.iterrows())[:20]:
         river = row['river_main_name']
         dest = row['river_dest']
+
         # start point
         if len(stack) == 0:
-            stack.append(dest)
+            stack.push(dest)
+            stack.push(river)
+        else:
+            assert dest in stack, "'{}' not in {}".format(dest, stack)
+            while dest != stack[-1]:
+                print stack
+                stack.pop()
+            stack.push(river)
 
-        stack.append(river)
-        print index[1], len(stack), "->".join([s.decode('utf8') for s in stack])
-        if dest == stack[-2]:
-            stack.pop()
+        print index[1], len(stack), stack
 
 
 def main():
     fname = sys.argv[1]
     df = pd.read_csv(fname, sep=';')
     df = prepare(df)
-    construct(df)
 
     if __debug__:
         global _df
         _df = df
+
+    construct(df)
 
 if __name__ == "__main__":
     main()
